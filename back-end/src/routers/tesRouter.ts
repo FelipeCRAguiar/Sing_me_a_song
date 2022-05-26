ae8365e3b81d5408e933c1f6c3e756a093c4a1d9
Celso <celsolnv27@gmail.com>
2022-05-26 10:49:50 -0300

    Preparando revisão de código

--- back-end/src/routers/tesRouter.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import * as testController from "../controllers/testController.js";
-const testRouter = Router();
-
-testRouter.delete("/recommendations", testController.clearRecommendations);
-
-export default testRouter;
